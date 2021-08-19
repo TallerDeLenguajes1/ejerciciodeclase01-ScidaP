741d8337be44eb79e03ef4ccd0ca9cd09912972e
Patricio Scidá <patricioscida@gmail.com>
2021-08-19 20:57:27 -0300

    Agregado try catch

--- Problema02/Program.cs
+++ Problema02/Program.cs
@@ -11,9 +11,13 @@ namespace Problema02
 
             num1 = 30;
             num2 = 60;
-            result = Convert.ToByte(num1 * num2);
-            Console.WriteLine("{0} x {1} = {2}", num1, num2, result);
-            Console.ReadLine();
+            try {
+                result = Convert.ToByte(num1 * num2);
+                Console.WriteLine("{0} x {1} = {2}", num1, num2, result);
+                Console.ReadLine();
+            } catch (Exception Ex) {
+                Console.WriteLine($"Mensaje de error: {Ex.Message}");
+            }
         }
     }
 }
