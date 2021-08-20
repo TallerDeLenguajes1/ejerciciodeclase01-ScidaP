d6b062667d2e30dfd2a455e876e0c87c53fa1f41
Patricio Scidá <patricioscida@gmail.com>
2021-08-19 21:14:41 -0300

    Solucionado

--- Problema04/Program.cs
+++ Problema04/Program.cs
@@ -8,7 +8,7 @@ namespace Problema04
         {
             int a = 1;
             int b = 2;
-            Console.WriteLine("{0} - {1} - {2}", a, b);
+            Console.WriteLine("{0} - {1}", a, b);
             Console.ReadLine();
         }
     }
